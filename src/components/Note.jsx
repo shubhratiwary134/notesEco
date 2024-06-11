741b900b3fd5fe8416c871c31098b675636eaec4
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-11 18:16:38 +0530

    background image added for the note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,15 @@
+
 export default function Note({title,description,id}){
-    console.log(id)
+    
     return (
         <>
-        <div style={{width:'200px', height:'200px'}}>
-        <h1>{title}</h1>
+        <div className="note-container">
+            <div style={{padding:'10em'}}> 
+            <h1>{title}</h1>
         <p>{description}</p>
+        <button >delete</button>
+            </div>
+        
         </div>
             
         </>
