b2c41422458bf4407f97665471fd7c5a5ad5c424
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-17 01:08:25 +0530

    background for the loader changed

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -2,7 +2,7 @@
 import loadingGif from '../assets/Illustration.gif'
 export const LoadingPage = () => {
   return (
-    <div className='flex justify-center items-center w-screen h-screen'>
+    <div className='flex justify-center items-center w-screen h-screen bg-[#F4F4F4]'>
         <img src={loadingGif} className='  md:h-auto'/>
     </div>
   )
