53620a752da0847259677b4895031a4ce2e40b77
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-17 04:35:05 +0530

    rounded the modal

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -49,7 +49,7 @@ export default function Note({title,description,id,handleDeleteNotes,pinned,refr
          isOpen={modalState}
         onRequestClose={closeModal}
         contentLabel='Modal'
-         className="modal "
+         className="modal rounded-xl sm:rounded-none"
          overlayClassName="modal-overlay"
         >
             <motion.div
